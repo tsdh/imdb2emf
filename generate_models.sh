7ed1d5f685e245a257f2ca5f7106cee70d97f760
Tassilo Horn <tsdh@gnu.org>
2014-01-24 08:57:47 +0100

    5000 steps

--- generate_models.sh
+++ generate_models.sh
@@ -1,16 +1,7 @@
 #!/bin/sh
 
-lein run imdb 100
-lein run imdb 250
-lein run imdb 500
-lein run imdb 1000
-lein run imdb 2500
-lein run imdb 5000
-lein run imdb 10000
-lein run imdb 25000
-lein run imdb 50000
-lein run imdb 100000
-lein run imdb 250000
-lein run imdb 500000
-lein run imdb 750000
-lein run imdb
+i=5000;
+while ((i < 800000)); do
+    lein run imdb $i
+    i=$((i + 5000))
+done
