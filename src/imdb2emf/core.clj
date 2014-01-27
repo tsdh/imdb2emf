430132f5c0aabfa86a124787f59c5c3714262b2c
Tassilo Horn <tsdh@gnu.org>
2014-01-27 15:52:29 +0100

    Allow creation of TGraphs instead of EMF models

--- src/imdb2emf/core.clj
+++ src/imdb2emf/core.clj
@@ -3,6 +3,7 @@
             [clojure.string    :as str]
             [funnyqt.utils     :as u]
             [funnyqt.emf       :as emf]
+            [funnyqt.tg        :as tg]
             [funnyqt.generic   :as g])
   (:import (java.util.zip GZIPInputStream)
            (java.io File)))
@@ -31,10 +32,6 @@
 
 (def ^:dynamic ^:private *movies-map*)
 (def ^:dynamic ^:private *model*)
-(def movietype-video     (emf/eenum-literal 'MovieType.VIDEO))
-(def movietype-videogame (emf/eenum-literal 'MovieType.VIDEOGAME))
-(def movietype-tv        (emf/eenum-literal 'MovieType.TV))
-(def movietype-movie     (emf/eenum-literal 'MovieType.MOVIE))
 
 (def movie-rx #"^([^\"\(]+)\s(\(\d\d\d\d(?:/[IVXCM]+)?\))\s(\([A-Z]+\))?\s+(\d\d\d\d)$")
 (defn parse-movies [file-name max-movie-count]
@@ -46,18 +43,17 @@
       (when match
         (swap! i inc)
         (let [[_ title year-n type year] match
-              movie (emf/ecreate! *model* 'Movie
-                                  :title title
-                                  :year  (Integer/parseInt year)
-                                  :type  (case type
-                                           "(V)"  movietype-video
-                                           "(VG)" movietype-videogame
-                                           "(TV)" movietype-tv
-                                           nil    movietype-movie))
+              movie (g/create-element!
+                     *model* 'Movie
+                     {:title title
+                      :year  (Integer/parseInt year)
+                      :type  (case type
+                               "(V)"  (g/enum-constant *model* 'MovieType.VIDEO)
+                               "(VG)" (g/enum-constant *model* 'MovieType.VIDEOGAME)
+                               "(TV)" (g/enum-constant *model* 'MovieType.TV)
+                               nil    (g/enum-constant *model* 'MovieType.MOVIE))})
               movie-id (str/trim (str title " " year-n (when type (str " " type))))]
-          (swap! *movies-map* assoc
-                 movie-id
-                 movie)
+          (swap! *movies-map* assoc movie-id movie)
           (when +verbose+
             (println (str "Movie: " title " (" year ") " type))))))
     @i))
@@ -80,9 +76,9 @@
             (when (and @current-actor (seq @current-movies))
               (swap! i inc)
               (locking *model*
-                (emf/ecreate! *model* cls
-                              :name   actor-name
-                              :movies @current-movies)
+                (g/create-element! *model* cls
+                                   {:name   actor-name
+                                    :movies @current-movies})
                 (when +verbose+
                   (println (str cls ": " actor-name " \t=> " (count @current-movies) " Movie(s)")))))
             (reset! current-actor  actor-name)
@@ -103,46 +99,22 @@
           (when-let [movie (@*movies-map* movie-id)]
             (swap! i inc)
             (locking *model*
-              (emf/eset! movie :rating (Double/parseDouble rating))
+              (g/set-aval! movie :rating (Double/parseDouble rating))
               (when +verbose+
                 (println (str "Rating: " movie-id "\t=> " rating))))))))
     @i))
 
-(defn print-class-counts []
-  (let [counts (atom {})]
-    (doseq [el (emf/eallobjects *model*)]
-      (swap! counts (fn [m]
-                      (assoc m (emf/eclass el)
-                             (if-let [c (m (emf/eclass el))]
-                               (inc c)
-                               1)))))
-    (println)
-    (println "Class counts")
-    (println "============")
-    (println)
-    (doseq [[cls cnt] @counts]
-      (println (str "#" (g/qname cls) ": " cnt)))
-    (println (reduce + (vals @counts)) "elements in total")
-    (println)))
-
-(defn parse-imdb [dir max-movie-count]
-  (binding [*model*      (emf/new-resource)
+(defn parse-imdb [kind dir max-movie-count]
+  (binding [*model*      (case kind
+                           :tg (tg/new-graph (tg/load-schema (io/resource "movies.tg")))
+                           :emf (emf/new-resource)
+                           (u/errorf "kind must be :tg or :emf but was %." kind))
             *movies-map* (atom {})]
     (let [cmovies       (parse-movies (pick-file dir "movies") max-movie-count)
           fut-actors    (future (parse-actors  (pick-file dir "actors")))
           fut-actresses (future (parse-actors  (pick-file dir "actresses")))
           fut-ratings   (future (parse-ratings (pick-file dir "ratings")))
           [cactors cactresses cratings] [@fut-actors @fut-actresses @fut-ratings]]
-      ;;(print-class-counts)
-      (println)
-      (println "Movies with zero actors:")
-      (let [i (atom 0)]
-        (doseq [[mid m] (filter (fn [[id m]]
-                                  (zero? (count (emf/eget m :persons))))
-                                @*movies-map*)]
-          (swap! i inc)
-          (println (str "  - '" mid "'")))
-        (println @i "movies with zero actors in total."))
       (println)
       (println "Parsed" cmovies "movies with" cactors "actors,"
                cactresses "actresses, and"
--- src/imdb2emf/main.clj
+++ src/imdb2emf/main.clj
@@ -1,25 +1,41 @@
 (ns imdb2emf.main
-  (:require [imdb2emf.core            :as i2e]
+  (:require [clojure.string :as str]
+            [imdb2emf.core            :as i2e]
             [imdb2emf.fast-xmi-export :as xmi]
             [imdb2emf.serialize       :as bin]
-            [funnyqt.emf              :as emf])
+            [funnyqt.emf              :as emf]
+            [funnyqt.tg               :as tg])
   (:gen-class))
+
+(defn elem-count [m]
+  (if (tg/graph? m)
+    (tg/vcount m)
+    (count (emf/eallobjects m))))
+
+(defn save-model [m fname]
+  (if (tg/graph? m)
+    (tg/save-graph m (str fname ".tg.gz"))
+    (do
+      (xmi/save-movies-model m fname)
+      (bin/save-movies-model m (str fname ".bin")))))
 
 (defn -main [& args]
   (when (or (zero? (count args))
-            (> (count args) 2))
-    (println "Usage: lein run <imdb-dir>")
-    (println "       lein run <imdb-dir> <max-movie-count>")
+            (> (count args) 3))
+    (println "Usage: lein run <kind> <imdb-dir>")
+    (println "       lein run <kind> <imdb-dir> <max-movie-count>")
+    (println "<kind> should either be EMF to create EMF models or TG to create TGraphs.")
+    (println "<imdb-dir> is the directory containing the IMDb files.")
+    (println "<max-movie-count> is the maximum number of movies to be parsed.")
     (System/exit 1))
-  (let [[imdb-dir max-movie-count] args
+  (let [[kind imdb-dir max-movie-count] args
         max-movie-count (if max-movie-count
                           (Integer/parseInt max-movie-count)
                           -1)
-        model (i2e/parse-imdb imdb-dir max-movie-count)
+        model (i2e/parse-imdb (keyword (str/lower-case kind)) imdb-dir max-movie-count)
         fname (format "imdb-%s-%s.movies"
                       (if (== -1 max-movie-count)
                         "all"
                         (format "%07d" max-movie-count))
-                      (count (emf/eallobjects model)))]
-    (xmi/save-movies-model model fname)
-    (bin/save-movies-model model (str fname ".bin"))))
+                      (elem-count model))]
+    (save-model model fname)))
