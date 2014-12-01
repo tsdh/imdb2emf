eb36d6c416252b577a9716b52d312e6970431bee
Tassilo Horn <tsdh@gnu.org>
2014-12-01 15:14:38 +0100

    Release 1.3.0

--- project.clj
+++ project.clj
@@ -1,11 +1,10 @@
-(defproject imdb2emf "1.2.0"
+(defproject imdb2emf "1.3.0"
   :description "A Parser from IMDb files to EMF models or JGraLab TGraphs."
   :url "https://github.com/tsdh/imdb2emf"
   :license {:name "GNU General Public License"
             :url "http://www.gnu.org/licenses/gpl.html"
             :distribution :repo}
-  :dependencies [[org.clojure/clojure "1.7.0-alpha4"]
-                 [funnyqt "0.33.1"]]
+  :dependencies [[funnyqt "0.33.1"]]
   :jvm-opts ^:replace ["-Xmx6G"]
   :global-vars {*warn-on-reflection* true
                 *assert* false}
